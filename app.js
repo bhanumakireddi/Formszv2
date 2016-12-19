452f088730d6fc70fc2a7678bed2f824c45b1f4a
Magikminds <bhanu735@gmail.com>
2016-12-19 13:02:40 +0530

    sdfdsf
    asdsaf

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var crypto = require('crypto');
-
 //var multer  = require('multer');
 //importsvar routes = require('./routes/index');
 var login = require('./routes/login');
